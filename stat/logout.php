f17e0d77cf8eafc89517e8c9489961b1e34245c6
adhy <jogjawebdotin@yahoo.com>
2014-06-17 08:38:23 +0700

    update
    hapus

--- stat/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start();
-session_start();
-require("../inc/config.php");
-$rt = mysql_query("UPDATE ".tabel_user." SET status_online=0 WHERE user_id='".$SessionNya['id']."'") or die("Log error");
-session_destroy();
-Header("Location: index.php");
-?>
