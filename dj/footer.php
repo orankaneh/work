dc810b9183ae84b0142882937a822447df93353f
adhy <jogjawebdotin@yahoo.com>
2014-06-17 08:39:58 +0700

    lagi
    lagi

--- dj/footer.php
+++
@@ -1,6 +0,0 @@
-</td>
-</tr>
-</table>
-<center><span class="kecil">Copyrights 2007 CITRAWEB NUSA INFOMEDIA</span></center>
-</body>
-</html>
--- inc/footerAdmin_bakup.php
+++
@@ -1,6 +0,0 @@
-</td>
-</tr>
-</table>
-<center><span id="foot" class="kecil">Copyrights 2007 CITRAWEB NUSA INFOMEDIA</span></center>
-</body>
-</html>
